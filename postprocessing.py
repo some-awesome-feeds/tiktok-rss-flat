e480edbf812a15c71b24f24590760f44468fba55
needywindigo <one-shadow-78@icloud.com>
2026-02-09 11:35:51 +0000

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -53,7 +53,7 @@ async def user_videos():
             updated=None
             
             async with TikTokApi() as api:
-                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False, browser="webkit")
+                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False)
                 ttuser = api.user(user)
                 try:
                     user_data = await ttuser.info()
@@ -104,4 +104,5 @@ async def user_videos():
 
 if __name__ == "__main__":
     asyncio.run(user_videos())
+
 
